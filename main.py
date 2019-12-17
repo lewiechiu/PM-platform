999b81ff123c1a3520ed749e0e561fae39a40d71
lewiechiu <louiechiu@gmail.com>
2019-12-17 22:59:30 +0800

    Added html ready for hosting

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
 from flask import Flask, render_template
-app = Flask(__name__, template_folder="template")
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def starting():
     return render_template('main.html')
 
-@app.route('/salesman.html', methods=['GET'])
-def sales_page():
-    return render_template('salesman.html')
+@app.route('/<pageName>', methods=['GET'])
+def page(pageName):
+    return render_template(pageName)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
