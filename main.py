77716a569e691e5e7b19ab08ff8c9c9513f9f08a
lewiechiu <louiechiu@gmail.com>
2019-12-17 21:48:16 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from app import app
+
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="template")
+
+@app.route('/', methods=['GET', 'POST'])
+def starting():
+    return render_template('main.html')
+
+@app.route('/salesman.html', methods=['GET'])
+def sales_page():
+    return render_template('salesman.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
