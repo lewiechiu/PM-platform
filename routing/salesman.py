06a91b7b70714444b2c12e033f03678c4f09d682
nicksome168 <b05502058@ntu.edu.tw>
2020-01-14 18:11:11 +0800

    separate crew.py into swe.py and salesman.py

--- routing/salesman.py
+++ routing/salesman.py
@@ -8,13 +8,101 @@ salesman = Blueprint('salesman', __name__, template_folder='../templates')
     # Know what each salesman is accountable for. (sales item)
     # Set KPI for each of the salesmen.
 # Show the customer SalesMan are related to
-# Update the time period for the crew in the company (repeat)
-# Promote a crew to Manager or Senior SWE
+# Update the time period for the salesman in the company (repeat)
+# Promote a salesman to Manager or Senior salesman
+
+# Create salesman
+@salesman.route('/api/salesman/<int:salesman_id>', methods = ['POST'])
+def create_salesman(salesman_id):
+    # TODO
+    # Insert salesman with the the following params.
+    # find the max id
+    # SQL : 
+    cmd = "SELECT MAX(ID) FROM salesman"
+    response = connect.queryALL(a)
+    # SQL :
+    # INSERT INTO salesman(ID,name,ssn,title, salary,age,years_of_experience,address,gender)
+    # VALUES(request.json()["name"],request.json()["ssn"],request.json()["title"],request.json()["salary"],request.json()["age"],request.json()["yoe"],request.json()["address"],request.json()["gender"])
+    print(request.json())
+    # request.json() should have the following property.
+    # ["name", "ssn", "title", "salary", "year", "address", "gender"]
+    createsalesman(salesman_id, name, ssn, title, salary, year, address, gender)
+
+# show if the salesman can be promoted
+@salesman.route('/api/salesman/<int:salesman_id>', methods = ['GET'])
+def show_order_experience(salesman_id):
+    # TODO
+    # Check if salesman exist in DB.
+    exist = SalesmanExist(salesman_id)
+    if not exist:
+        return abort(400, "Salesman id: {} does NOT EXIST".format(salesman_id))
+    getSalesOrder(salesman_id)
+    return
+
+
+# Select certain Talents of the People
+@salesman.route('/api/salesman/<string:talent>', methods = ['GET'])
+def select_by_talent(talent):
+    #
+    pass
+    # TODO
+    # Select from salesman and salesman where Talent in the variable talent.
+    # You can use some formatting like
+    getsalesmanByTalent(talent)
+    # return the result selected
+
+
+# Show the background, basic info for each person.
+@salesman.route('/api/salesman/background', methods = ['GET'])
+def get_salesman_info(salesman_id):
+    pass
+    # TODO
+    getsalesmanInfo(salesman_id)
+    # Select the whole table.
+    #SQL:
+    #SELECT * FROM salesman
+    response = connect.queryAll('SELECT * FROM salesman')
+
+# Update
+# Update the time period for the salesman in the company
+@salesman.route('/api/salesman/time_period/<int:salesman_id>', methods = ['PUT'])
+def update_salesman(salesman_id):
+    # TODO
+    # In request body, there will be a field "period".
+    # Update the salesman/salesman having id with "years_of_experience" being "period"
+    exist_salesman = salesmanExist(salesman_id)
+    if not exist_salesman:
+        return abort(400, "salesman id: {} does NOT EXIST".format(salesman_id))
+    exist_sales = SalesmanExist(salesman_id)
+    if not exist_sales:
+        return abort(400, "Salesman id: {} does NOT EXIST".format(salesman_id))
+    if(exist_salesman):
+        update_salesman(salesman_id,  target_attri, new_value, 'salesman')
+    else:
+        update_salesman(salesman_id,  target_attri, new_value, 'sales')
+    pass
+
+# Delete
+@salesman.route('/api/salesman/<int:salesman_id>', methods = ['DELETE'])
+def delete_salesman(salesman_id):
+    # TODO
+    exist_salesman = salesmanExist(salesman_id)
+    if not exist_salesman:
+        return abort(400, "salesman id: {} does NOT EXIST".format(salesman_id))
+    exist_sales = SalesmanExist(salesman_id)
+    if not exist_sales:
+        return abort(400, "Salesman id: {} does NOT EXIST".format(salesman_id))
+    # Delete the record for salesman
+    if(exist_salesman):
+        changesalesmanState(salesman_id, 'salesman','retired')
+    else:
+        changesalesmanState(salesman_id, 'sales', 'retired')
+    pass
 
 
 
 
-
+#------------------------------------------------------------------
 @salesman.route('/api/sales/', methods = ['POST'])
 def sales_api():
     return jsonify({"status": 200})
--- routing/swe.py
+++ routing/swe.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template, jsonify, request, Blueprint, abort
 from mysql_conf import *
 from basic_function import *
 
-crew = Blueprint('crew', __name__, template_folder='../templates')
+swe = Blueprint('swe', __name__, template_folder='../templates')
 
 connect = MySQL_query()
 resp = connect.queryALL("SELECT ....")
 resp = [(), (), ...]
 [()]
 
-# Create Crew
-@crew.route('/api/crew/<int:crew_id>', methods = ['POST'])
-def create_crew(crew_id):
+# Create swe
+@swe.route('/api/swe/<int:swe_id>', methods = ['POST'])
+def create_swe(swe_id):
     # TODO
     # Insert SWE with the the following params.
     # find the max id
@@ -24,11 +24,11 @@ def create_crew(crew_id):
     print(request.json())
     # request.json() should have the following property.
     # ["name", "ssn", "title", "salary", "year", "address", "gender"]
-    createCrew(crew_id, name, ssn, title, salary, year, address, gender)
+    createswe(swe_id, name, ssn, title, salary, year, address, gender)
 
 '''
-# show if the crew can be promoted
-@crew.route('/api/crew/<int:swe_id>', methods = ['GET'])
+# show if the swe can be promoted
+@swe.route('/api/swe/<int:swe_id>', methods = ['GET'])
 def show_project_experience(swe_id):
     # TODO
     # Check if swe exist in DB.
@@ -38,8 +38,8 @@ def show_project_experience(swe_id):
     getsweproject(swe_id)
     return 
 '''
-# show if the crew can be promoted
-@crew.route('/api/crew/<int:salesman_id>', methods = ['GET'])
+# show if the swe can be promoted
+@swe.route('/api/swe/<int:salesman_id>', methods = ['GET'])
 def show_order_experience(salesman_id):
     # TODO
     # Check if swe exist in DB.
@@ -50,30 +50,30 @@ def show_order_experience(salesman_id):
     return
 
 # Select certain Talents of the People
-@crew.route('/api/crew/<string:talent>', methods = ['GET'])
+@swe.route('/api/swe/<string:talent>', methods = ['GET'])
 def select_by_talent(talent):
     #
     pass
     # TODO
-    # Select from swe and Crew where Talent in the variable talent.
+    # Select from swe and swe where Talent in the variable talent.
     # You can use some formatting like
-    getCrewByTalent(talent)
+    getsweByTalent(talent)
     # return the result selected
 
 
 # Show the background, basic info for each person.
-@crew.route('/api/crew/background', methods = ['GET'])
-def get_crew_info(crew_id):
+@swe.route('/api/swe/background', methods = ['GET'])
+def get_swe_info(swe_id):
     pass
     # TODO
-    getCrewInfo(crew_id)
+    getsweInfo(swe_id)
     # Select the whole table.
     #SQL:
     #SELECT * FROM SWE
     response = connect.queryAll('SELECT * FROM SWE')
 
 # Show what they are accountable for(Manage) or assigned to do(has a manager).
-@crew.route('/api/crew/<int:id>', methods = ['GET'])
+@swe.route('/api/swe/<int:id>', methods = ['GET'])
 def get_swe_manager(swe_id):
     pass
     # TODO
@@ -94,9 +94,9 @@ def get_swe_manager(swe_id):
 
     # Return them
 '''
-# Select Crew by Dev Team
-@crew.route('/api/crew/inTeam/<int:ID>', methods = ['GET'])
-def get_crew_manages(ID):
+# Select swe by Dev Team
+@swe.route('/api/swe/inTeam/<int:ID>', methods = ['GET'])
+def get_swe_manages(ID):
     pass
     # TODO
     # Select the SWE ID having Dev Team being ID.
@@ -115,12 +115,12 @@ def get_crew_manages(ID):
     # Return them
 
 # Update
-# Update the time period for the crew in the company
-@crew.route('/api/crew/time_period/<int:crew_id>', methods = ['PUT'])
-def update_crew(crew_id):
+# Update the time period for the swe in the company
+@swe.route('/api/swe/time_period/<int:swe_id>', methods = ['PUT'])
+def update_swe(swe_id):
     # TODO
     # In request body, there will be a field "period".
-    # Update the swe/crew having id with "years_of_experience" being "period"
+    # Update the swe/swe having id with "years_of_experience" being "period"
     exist_swe = SWEExist(swe_id)
     if not exist_swe:
         return abort(400, "SWE id: {} does NOT EXIST".format(swe_id))
@@ -128,14 +128,14 @@ def update_crew(crew_id):
     if not exist_sales:
         return abort(400, "Salesman id: {} does NOT EXIST".format(salesman_id))
     if(exist_swe):
-        update_crew(crew_id,  target_attri, new_value, 'swe')
+        update_swe(swe_id,  target_attri, new_value, 'swe')
     else:
-        update_crew(crew_id,  target_attri, new_value, 'sales')
+        update_swe(swe_id,  target_attri, new_value, 'sales')
     pass
 
 # Delete
-@crew.route('/api/crew/<int:crew_id>', methods = ['DELETE'])
-def delete_crew(crew_id):
+@swe.route('/api/swe/<int:swe_id>', methods = ['DELETE'])
+def delete_swe(swe_id):
     # TODO
     exist_swe = SWEExist(swe_id)
     if not exist_swe:
@@ -143,19 +143,19 @@ def delete_crew(crew_id):
     exist_sales = SalesmanExist(salesman_id)
     if not exist_sales:
         return abort(400, "Salesman id: {} does NOT EXIST".format(salesman_id))
-    # Delete the record for crew
+    # Delete the record for swe
     if(exist_swe):
-        changeCrewState(crew_id, 'swe','retired')
+        changesweState(swe_id, 'swe','retired')
     else:
-        changeCrewState(crew_id, 'sales', 'retired')
+        changesweState(swe_id, 'sales', 'retired')
     pass
 
-@crew.route('/api/crew/', methods = ['POST'])
-def crew_api():
+@swe.route('/api/swe/', methods = ['POST'])
+def swe_api():
     return jsonify({"status": 200})
 
 
-def createCrew(name, ssn, title, salary, year, address, gender):
+def createswe(name, ssn, title, salary, year, address, gender):
     return True
 
 
@@ -166,12 +166,12 @@ def getsweproject(swe_id):
 def getSalesOrder(salesman_id):
     return {}
 
-def getCrewByTalent(talent):
+def getsweByTalent(talent):
     # check swe and salesman table and see if they have the talent
     return {}
 
-def getCrewInfo(crew_id):
-    # return the crew info
+def getsweInfo(swe_id):
+    # return the swe info
     return {}
 
 def getManageProject(swe_id=None):
@@ -179,14 +179,14 @@ def getManageProject(swe_id=None):
     # else only get the specified one
     return{}
 
-def update_crew(crew_id,  target_attri, new_value, role)
+def update_swe(swe_id,  target_attri, new_value, role)
     # if role == 'swe'
         # update swe
     # else 
         # update sales
     return TRUE
 
-def changeCrewState(crew_id, role, state):
+def changesweState(swe_id, role, state):
     # if role == 'swe'
         # if he doesn't manage active project
             # update the swe state to 'retire'
