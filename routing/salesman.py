5241cf5824c11aa6adc8ca5bf5411647179cdb42
lewiechiu <louiechiu@gmail.com>
2020-01-14 18:01:36 +0800

    Salesman backbone

--- routing/salesman.py
+++ routing/salesman.py
@@ -4,12 +4,11 @@ salesman = Blueprint('salesman', __name__, template_folder='../templates')
 
 
 
-# show the ranking of each salesman by performance.
-# Know what each salesman is accountable for. (sales item)
-# Set KPI for each of the salesmen.
-# Show the customer SalesMan are related to
 
-# Update the time period for the crew in the company
+    # Know what each salesman is accountable for. (sales item)
+    # Set KPI for each of the salesmen.
+# Show the customer SalesMan are related to
+# Update the time period for the crew in the company (repeat)
 # Promote a crew to Manager or Senior SWE
 
 
@@ -20,8 +19,16 @@ salesman = Blueprint('salesman', __name__, template_folder='../templates')
 def sales_api():
     return jsonify({"status": 200})
 
+# show the ranking of each salesman by performance.
 @salesman.route('/api/sales/ranking/', methods = ['GET'])
 def sales_ranking(id):
-    # Query all the salesman id and their performance status?
+    # We use the number of projects and a salesman has handled as the KPI.
+    # This can be decided afterwards.
+    # TODO:
+    # 1. Query all the salesman id
+    # For each of the salesman id, query their "KPI" data.
+    # sort the result by some 
     # sort by the performance ranking 
     return 
+
+@salesman.route('/api/sales/')
