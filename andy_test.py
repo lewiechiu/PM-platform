ab13d65e471e4a680179260b5e36852e57314e60
superandy <superandy0407@gmail.com>
2020-01-15 23:51:21 +0800

    get available error

--- andy_test.py
+++ andy_test.py
@@ -1,6 +1,7 @@
 from mysql_conf import *
 import json
 import requests
+from tabulate import tabulate
 connect = MySQL_query()
 
 # # state = " notstart"
@@ -165,6 +166,104 @@ connect = MySQL_query()
 # # printing the output 
 # print(int(id))
 # print(task)
+
+
+# URL = "http://127.0.0.1:5000/api/project/new_projects"
+# print("Please enter SWE ID : ")
+# swe_id = input()
+# print("Please enter Manager ID : ")
+# manage_id = input()
+# print("Please enter Project ID : ")
+# project_id = input()
+# print("Please enter Order ID : ")
+# order_id = input()
+# print("Please enter State : ")
+# state = input()
+# swe_id = swe_id.split(" ")
+# data_send = {"SWE ID":swe_id,"Manager ID":manage_id,"Project ID":project_id,"Order ID":order_id,"State":state}
+# r = requests.post(URL,json = data_send)
+# if r.status_code == 200:
+#      print("successfully added!")
+
+
+# URL = "http://127.0.0.1:5000/api/project/task/"
+# print("Please enter project ID : ")
+# project_id = input()
+# URL = URL + project_id
+# print("Please enter Resources : ")
+# resource = input()
+# print("Please enter State : ")
+# state = input()
+# print("Please enter Category : ")
+# category = input()
+# print("Please enter Resource Amount : ")
+# r_amount = input()
+# data_send = {"project ID":project_id,"Resources":resource,"State":state,"Category":category,"Resource Amount":r_amount}
+# r = requests.post(URL,json = data_send)
+# if r.status_code == 200:
+#     print("successfully added!")
+
+
+# URL = "http://127.0.0.1:5000/api/project/active"
+# r = requests.get(URL)
+# response = r.json()
+# print(response["No_id"]," projects")
+# cnt = 0
+# print_dataList = []
+# while cnt < len(response["Projects"]):
+#     print_dataList.append(response["Projects"][cnt:cnt+5])
+#     cnt = cnt + 5
+# print (tabulate(print_dataList))
+
+# URL = "http://127.0.0.1:5000/api/project/active/" + "833"
+# r = requests.get(URL)
+# response = r.json()
+# print(response["project_id"]," is the project's ID")
+# cnt = 0
+# print_dataList = []
+# while cnt < len(response["task"]):
+#     print_dataList.append(response["task"][cnt:cnt+5])
+#     cnt = cnt + 5
+# print (tabulate(print_dataList,headers = ["Task"]))
+
+# print("Please enter Project ID : ")
+# project_id = input()
+# URL = "http://127.0.0.1:5000/api/project/talent/" + project_id
+# r = requests.get(URL)
+# response = r.json()
+# print_dataList = []
+# for i in range(len(response)):
+#     tempt = []
+#     tempt.append(response[i]["Name"])
+#     tempt.append(response[i]["Job title"])
+#     tempt.append(response[i]["Talent"])
+#     print_dataList.append(tempt)
+# print(tabulate(print_dataList,headers = ["Name","Job Title","Talent"]))
+# URL = "http://127.0.0.1:5000/api/project/availability/1001"
+# r = requests.get(URL)
+# print(r)
+# response = r.json()
+# print(response)
+# print_dataList = []
+# for i in range(len(response)):
+#     tempt = []
+#     tempt.append(response[i]["SWE_id"])
+#     tempt.append(response[i]["Name"])
+#     tempt.append(response[i]["Available capacity"])
+#     print_dataList.append(tempt)
+# print(tabulate(print_dataList,headers = ["SWE ID","Name","Avaliable Capacity"]))   
+
+
+
+cmd = "SELECT SWEID,Name FROM SWE"
+response = connect.queryALL(cmd)
+SWE_ID = clean_tuple(response,0)
+SWE_Name = clean_tuple(response,1)
+print(response)
+print(SWE_ID)
+print(SWE_Name)
+
+
 
 
 
